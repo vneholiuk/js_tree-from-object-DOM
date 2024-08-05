e4de3d07d310d8b95a7f62090157d28fc6ec95a9
Vlados <vnegoluk@gmail.com>
2024-08-05 15:56:43 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,36 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    // WRITE YOUR CODE HERE
+    const ul = document.createElement("ul");
+    element.appendChild(ul);
+    for (const item of Object.entries(data)){
+        const li = document.createElement("li");
+        li.textContent = item[0];
+        ul.appendChild(li);
+        if (item[1] && typeof item[1] === "object" & Object.keys(item[1]).length > 0) {
+            const nextUl = document.createElement("ul");
+            element.appendChild(nextUl);
+            createTree(li, item[1]);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
